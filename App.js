4c7cfed55520d471f1e5d339471d0761dbe41bea
Luis Manyari <lmanyari@hotmail.com>
2024-03-20 07:26:39 -0400

    Temporary chanmge

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import data from './data.json';
+
+// put a JSON file on the page
+// import data from './data.json';
+
+// console.log(data);
+
+const App = () => {
+  return (
+    <>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </>
+  );
+};
+
+export default App;
+
